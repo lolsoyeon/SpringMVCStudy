93fde6621d1916c63bbeb6eb33153642a978f242
lolsoyeon <107609375+lolsoyeon@users.noreply.github.com>
2022-12-04 22:56:04 +0900

    Student, Grade
    com.test.mydatis.mapper/GradeDAO, StudentDAO

---
+++ MybatisApp03/src/com/test/mybatis/IGradeDAO.java
@@ -0,0 +1,21 @@
+/*==========================
+ 	IGradeDAO.java
+ 	- 인터페이스
+ ==========================*/
+package com.test.mybatis;
+
+import java.util.ArrayList;
+
+public interface IGradeDAO
+{
+	// 성적 정보 인원 수 확인
+	public int count();
+	
+	// 성적 정보 리스트 확인
+	public ArrayList<GradeDTO> list();
+
+	// 성적 정보 입력(추가)
+	public int add(GradeDTO g);
+	
+	
+}
